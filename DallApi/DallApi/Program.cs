60b0eaeba91f1b41699a1b157ccb29396aabbb85
Andreas <89839661+Andreasd21@users.noreply.github.com>
2022-12-07 11:02:06 +0100

    fixed docker file

--- DallApi/DallApi/Program.cs
+++ DallApi/DallApi/Program.cs
@@ -5,7 +5,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSignalR(options =>
+{
+    options.EnableDetailedErrors = true;
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
