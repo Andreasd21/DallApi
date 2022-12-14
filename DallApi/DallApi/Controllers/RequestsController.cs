c6207ec9fd45d6e9f7d15e4b26dd69149fb82e35
Andreas <89839661+Andreasd21@users.noreply.github.com>
2022-12-14 15:10:15 +0100

    Fixed request trouble

--- DallApi/DallApi/Controllers/RequestsController.cs
+++ DallApi/DallApi/Controllers/RequestsController.cs
@@ -28,6 +28,16 @@ namespace DallApi.Controllers
             return await _context.Requests.ToListAsync();
         }
 
+        // GET: api/Requests
+        [HttpGet("GetPending")]
+        public async Task<ActionResult<IEnumerable<Request>>> GetPendingRequests()
+        {
+            var request = await _context.Requests
+                                        .Where(r => r.Status == "pending")
+                                        .ToListAsync();
+            return request;
+        }
+
         // GET: api/Requests/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Request>> GetRequest(int id)
